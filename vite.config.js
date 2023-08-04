50aa1132777b1ab5e399d7f8a16355be94640d45
Mookth789 <135083436+Mookth789@users.noreply.github.com>
2023-08-04 07:02:42 +0700

    3 files modified

--- vite.config.js
+++ vite.config.js
@@ -7,17 +7,16 @@ import NutUIResolver from '@nutui/nutui/dist/resolver'
 export default defineConfig({
   plugins: [
     vue(),
-    // 开启 unplugin 插件，自动引入 NutUI 组件
-    Components({
-      resolvers: [NutUIResolver()],
-    }),
-  ],
-  // 配置全局样式变量
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `@import "@nutui/nutui/dist/styles/variables.scss";`
-      }
+  Components({
+    resolvers: [NutUIResolver()],
+  }),
+],
+// 配置全局样式变量
+css: {
+  preprocessorOptions: {
+    scss: {
+      additionalData: `@import "@nutui/nutui/dist/styles/variables.scss";`
     }
   }
+}
 })
