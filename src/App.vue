449bf4af71d6015db9ae4fa3fd4c661574ebbfa8
Mookth789 <135083436+Mookth789@users.noreply.github.com>
2023-08-04 09:58:42 +0700

    2 files modified

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,39 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <nut-tabbar bottom safe-area-inset-bottom placeholder>
+    <nut-tabbar-item v-for="(item,index) in List" :tab-title="item.title" :icon="item.icon"> </nut-tabbar-item>
+  </nut-tabbar>
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<script>
+  import { h } from 'vue';
+  import { Home, Category, Find, Cart, My } from '@nutui/icons-vue';
+  export default {
+    components: { Home, Category, Find, Cart, My },
+    setup() {
+      const List = [
+        {
+          title: '标签',
+          icon: h(Home)
+        },
+        {
+          title: '标签',
+          icon: h(Category)
+        },
+        {
+          title: '标签',
+          icon: h(Find)
+        },
+        {
+          title: '标签',
+          icon: h(Cart)
+        },
+        {
+          title: '标签',
+          icon: h(My)
+        }
+      ];
+      return {
+        List
+      };
+    }
+  };
+</script>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from 'vue';
+import { Tabbar, TabbarItem } from '@nutui/nutui';
+import { IconFont } from '@nutui/icons-vue';
+import '@nutui/icons-vue/dist/style_iconfont.css';
 
-createApp(App).mount('#app')
+const app = createApp();
+app.use(Tabbar, TabbarItem, IconFont);
